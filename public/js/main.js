1a3374f28aa642fce7125a170ce2e3a91adeed4c
Alexis THOMAS <fr.alexisthomas@gmail.com>
2014-05-29 01:19:25 +0200

    factory

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,9 @@
 var booksApp = angular.module("booksApp", []);
 
-var MainCtrl = booksApp.controller("MainCtrl", function($scope) {
-    $scope.books = [
-        {
-            title: "Backbone c'est de la balle",
-            description: "tutorial bb",
-            level: "très bon"
-        },
-        {
-            title: "React ça dépote",
-            description: "se perfectionner avec React",
-            level: "bon"
-        },
-        {
-            title: "J'apprends Angular",
-            description: "from scratch",
-            level: "débutant"
-        }
-    ];
+booksApp.controller("MainCtrl", function($scope, Models) {
 
-    $scope.levels = [
-        "très bon", "bon", "débutant"
-    ];
+    $scope.books = Models.books();
+    $scope.levels = Models.levels();
 
     $scope.selectedBook = null;
 
@@ -36,4 +18,37 @@ var MainCtrl = booksApp.controller("MainCtrl", function($scope) {
             level: "???"
         });
     };
+});
+
+booksApp.factory("Models", function() {
+    var books = function() {
+        return [
+            {
+                title: "Backbone c'est de la balle",
+                description: "tutorial bb",
+                level: "très bon"
+            },
+            {
+                title: "React ça dépote",
+                description: "se perfectionner avec React",
+                level: "bon"
+            },
+            {
+                title: "J'apprends Angular",
+                description: "from scratch",
+                level: "débutant"
+            }
+        ];
+    };
+
+    var levels = function() {
+        return [
+            "très bon", "bon", "débutant"
+        ];
+    };
+
+    return {
+        books: books,
+        levels: levels
+    }
 });
