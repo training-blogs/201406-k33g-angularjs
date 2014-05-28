918b1305aa9d59c8de18cf6f526bf1dda5e8dee8
Alexis THOMAS <fr.alexisthomas@gmail.com>
2014-05-29 00:58:44 +0200

    architecture

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,21 @@
-// Foo ...
+var booksApp = angular.module("booksApp", []);
+
+var MainCtrl = booksApp.controller("MainCtrl", function($scope) {
+    $scope.books = [
+        {
+            title: "Backbone c'est de la balle",
+            description: "tutorial bb",
+            level: "très bon"
+        },
+        {
+            title: "React ça dépote",
+            description: "se perfectionner avec React",
+            level: "bon"
+        },
+        {
+            title: "J'apprends Angular",
+            description: "from scratch",
+            level: "débutant"
+        }
+    ];
+});
