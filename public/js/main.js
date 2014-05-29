3d47d4dc033bc04d533d4387843368518ea8a395
Alexis THOMAS <fr.alexisthomas@gmail.com>
2014-05-30 01:06:57 +0200

    service

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,5 @@
 var booksApp = angular.module("booksApp", []);
 
-booksApp.controller("MainCtrl", function($scope, $http, Models) {
-    $scope.books = null;
-    $scope.levels = Models.levels();
-
-    $scope.getAllBooks = function() {
-        $http.get("books").success(function(data) {
-            $scope.books = data;
-        });
-    };
-
-    $scope.createBook = function(book) {
-        $http.post("books", book).success(function(data){
-            $scope.getAllBooks();
-            $scope.book = null;
-        });
-    };
-
-    $scope.getAllBooks();
-});
-
 booksApp.factory("Models", function() {
     var levels = function() {
         return [
@@ -30,4 +10,37 @@ booksApp.factory("Models", function() {
     return {
         levels: levels
     }
+});
+
+booksApp.service("BooksServices", function($http) {
+    this.getAll = function(callback) {
+        $http.get("books").success(function(data) {
+            callback(data);
+        })
+    };
+
+    this.create = function(book, callback) {
+        $http.post("books", book).success(function(data){
+            callback(data);
+        });
+    };
+});
+
+booksApp.controller("MainCtrl", function($scope, Models, BooksServices) {
+    $scope.books = null;
+    $scope.levels = Models.levels();
+
+    $scope.getAllBooks = function() {
+        BooksServices.getAll(function(data) {
+            $scope.books = data;
+        });
+    };
+
+    $scope.createBook = function(book) {
+        BooksServices.create(book, function(data) {
+            $scope.getAllBooks();
+        });
+    };
+
+    $scope.getAllBooks();
 });
